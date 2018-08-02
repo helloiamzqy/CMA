7f2eba8a2162fdd327a122f20ce88802926b2e71
CHENJA25 <jason.chen@oocl.com>
2018-08-02 08:33:18 +0800

    Merge branch 'master' of https://github.com/typhoonTeam/CMA into merchant
    # Conflicts:
    #	merchant/src/main/java/service/impl/UserServiceImpl.java

---
+++ merchant/src/main/java/dao/MerchantDao.java
@@ -0,0 +1,11 @@
+package dao;
+
+import pojo.Merchant;
+
+import java.util.List;
+
+public interface MerchantDao {
+    public Merchant addMerchant(Merchant merchant);
+    public List<Merchant> findMerchant();
+    public Merchant updateMerchant(Merchant merchant);
+}
