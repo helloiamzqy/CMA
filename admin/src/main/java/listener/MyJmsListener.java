5d47f22e078ef5f1a1ec0b337e618ba60c923ecf
JohnGao <john.gao@oocl.com>
2018-08-02 10:25:53 +0800

    测试监听器

--- admin/src/main/java/listener/MyJmsListener.java
+++ admin/src/main/java/listener/MyJmsListener.java
@@ -1,6 +1,9 @@
 package listener;
 
 import org.springframework.stereotype.Component;
+import pojo.Complaint;
+import utils.JsonParse;
+import utils.JsonParseByJackson;
 
 import javax.jms.JMSException;
 import javax.jms.Message;
@@ -10,11 +13,15 @@ import javax.jms.TextMessage;
 @Component(value = "myListener")
 public class MyJmsListener implements MessageListener {
 
-    public void onMessage(Message message) {
+    public void onMessage(Message message){
         TextMessage tmsg = (TextMessage)message;
         try {
-            System.out.println("my listener receive:" + tmsg.getText());
-        }catch (JMSException e){
+//            tmsg.getText()
+            System.out.println("中文");
+            System.out.println("my listener receive:" +new String(tmsg.getText().getBytes("iso8859-1"),"UTF-8"));
+            JsonParse<Complaint> parse = new JsonParseByJackson<>();
+            Object object = parse.parseJsonToObject(Complaint.class,tmsg.getText());
+        }catch (Exception e){
             e.printStackTrace();
         }
     }
