3505e6f0a81d39cccb3bd4da0f46491505aa8996
ZHENGDU <dunn.zheng@oocl.com>
2018-08-02 22:27:32 +0800

    basicinfo接口编写

--- merchant/src/main/java/dao/impl/AdvertisementDaoImpl.java
+++ merchant/src/main/java/dao/impl/AdvertisementDaoImpl.java
@@ -2,6 +2,7 @@ package dao.impl;
 
 import dao.AdvertisementDao;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import pojo.Advertisement;
 import pojo.Merchant;
 
@@ -14,6 +15,8 @@ import java.util.List;
 public class AdvertisementDaoImpl implements AdvertisementDao {
     @PersistenceContext(name = "em")
     private EntityManager manager;
+
+    @Transactional
     @Override
     public Advertisement addAdvertisement(Advertisement advertisement) {
         manager.persist(advertisement);
@@ -22,10 +25,20 @@ public class AdvertisementDaoImpl implements AdvertisementDao {
 
     @Override
     public List<Advertisement> findAdvertisementByMerchant(Merchant merchant) {
-        String jpql="from pojo.Advertisement ad where ad.merchant=:merchant";
+        String jpql="FROM pojo.Advertisement ad where ad.merchant=:merchant";
         Query query=manager.createQuery(jpql);
         query.setParameter("merchant",merchant);
         List<Advertisement> advertisements=query.getResultList();
         return advertisements;
     }
+
+    @Override
+    public List<Advertisement> findAdvertisementById(List<String> ids) {
+        String jpql = "FROM pojo.Advertisement ad WHERE ad.id in(:ids)";
+        Query query=manager.createQuery(jpql);
+        query.setParameter("ids",ids);
+        List<Advertisement> advertisements=query.getResultList();
+        return advertisements;
+    }
+
 }
