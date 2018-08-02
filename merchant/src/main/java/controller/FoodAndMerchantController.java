8435fffccd97954129718bad3782a63558798d5a
ZHENGDU <dunn.zheng@oocl.com>
2018-08-02 14:18:11 +0800

    food 分页

--- merchant/src/main/java/controller/FoodAndMerchantController.java
+++ merchant/src/main/java/controller/FoodAndMerchantController.java
@@ -3,6 +3,8 @@ package controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import pojo.Food;
+import pojo.Merchant;
 import pojo.Pager;
 import service.FoodManger;
 import service.MerchantManager;
@@ -24,7 +26,18 @@ public class FoodAndMerchantController {
         return foodManger.findAllFood(curPage,pageSize);
 
     }
-
+    @RequestMapping(method = RequestMethod.POST,value = "/merchant/{id}/food")
+    public Food addFood(@PathVariable String id,Food food){
+        Merchant merchant = new Merchant();
+        merchant.setId(id);
+        food.setMerchant(merchant);
+        foodManger.addFood(food);
+        return food;
+    }
+    @RequestMapping(method=RequestMethod.DELETE,value = "/merchant/{id}/food")
+    public void delete(@PathVariable String id,Food food){
+        
+    }
 
 
 }
