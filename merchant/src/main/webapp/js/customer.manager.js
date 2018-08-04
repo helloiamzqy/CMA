3f0bc7610dd3e314fae6cf277a331e1f3ff15c44
CHENJA25 <jason.chen@oocl.com>
2018-08-04 12:26:29 +0800

    20180804 订单详情改动

--- merchant/src/main/webapp/js/customer.manager.js
+++ merchant/src/main/webapp/js/customer.manager.js
@@ -9,84 +9,85 @@ function CustomerComponent($view,host,mId,status) {
             status=$("#down").val();
             myAjax(host+getUrl+status+page,"GET",null,(orders)=>{
                 model=orders.list;
-                render();
-            });
+            render();
+        });
         })
         $view.find("#addForm").on("submit",(e)=>{
             e.preventDefault();
-            let obj=$(e.target).serializeObject();
-            myAjax(url,"POST",obj,(nc)=>{
-                model.push(nc);
-                render();
-            })
-        });
+        let obj=$(e.target).serializeObject();
+        myAjax(url,"POST",obj,(nc)=>{
+            model.push(nc);
+        render();
+    })
+    });
         myAjax(host+getUrl+status+page,"GET",null,(orders)=>{
-            model=orders.list;
-            render();
-        });
+            alert(host+getUrl+status+page);
+        model=orders.list;
+        render();
+    });
     }
     function render() {
         let $tbody=$view.find("#orderList tbody");
         $tbody.empty();
         model.forEach((order)=>{
             let createTime=timeStamp2String(order.createTime);
-            let finishTime=timeStamp2String(order.finishTime);
-            let status=judgeStatus(order.status);
-            let tr=$("<tr>").attr("id",order.id)
-                .append($("<td>").text(order.id))
-                .append($("<td>").text(order.customer.name))
-                .append($("<td>").text(order.customer.phone))
-                .append($("<td>").text(order.customer.address))
-                .append($("<td>").text(order.totalPrice))
-                .append($("<td>").text(createTime))
-                .append($("<td>").text(finishTime))
-                .append($("<td>").append($("<button>").attr("data-am-modal","{target:'#my-popup'}").addClass("am-round").text("查看详情").on("click",(e)=>{
-                    getOrderItem(order.id);
-                    e.preventDefault();
-                })))
-                .append($("<td>").text(status)).appendTo($tbody);
-            if(order.status=="0"){
-                tr.append($("<td>").append(($("<button>").addClass("am-round")).text("接单").on("click",(e)=>{
-                    e.preventDefault();
-                    changeOrder(order,1)
-                })))
-                .append($("<td>").append(($("<button>").addClass("am-round")).text("拒绝").on("click",(e)=>{
-                    e.preventDefault();
-                    changeOrder(order,0)
-                })))
-            }else if(order.status=="1"){
-                tr.append($("<td>").append(($("<button>").addClass("am-round")).text("配送").on("click",(e)=>{
-                    e.preventDefault();
-                    changeOrder(order,2)
-                })))
-            }else if(order.status=="2"){
-                tr.append($("<td>").append($("<button>").addClass("am-round").text("完成").on("click",(e)=>{
-                    e.preventDefault();
-                    changeOrder(order,3)
-                })))
-            }else if(order.status=="3"){
-                tr.append($("<td>").text("已完成"));
-            }else if(order.status=="4"){
-                tr.append($("<td>").text("已拒绝接单"));
-            }else {
-                tr.append($("<td>").text("接单已取消"));
-            }
-        })
+        let finishTime=timeStamp2String(order.finishTime);
+        let status=judgeStatus(order.status);
+        let tr=$("<tr>").attr("id",order.id)
+            .append($("<td>").text(order.id))
+            .append($("<td>").text(order.customer.name))
+            .append($("<td>").text(order.customer.phone))
+            .append($("<td>").text(order.customer.address))
+            .append($("<td>").text(order.totalPrice))
+            .append($("<td>").text(createTime))
+            .append($("<td>").text(finishTime))
+            .append($("<td>").append($("<button>").attr("data-am-modal","{target:'#my-popup'}").addClass("am-round").text("查看详情").on("click",(e)=>{
+                getOrderItem(order.id);
+        e.preventDefault();
+    })))
+    .append($("<td>").text(status)).appendTo($tbody);
+        if(order.status=="0"){
+            tr.append($("<td>").append(($("<button>").addClass("am-round")).text("接单").on("click",(e)=>{
+                e.preventDefault();
+            changeOrder(order,1)
+        })))
+        .append($("<td>").append(($("<button>").addClass("am-round")).text("拒绝").on("click",(e)=>{
+                e.preventDefault();
+            changeOrder(order,0)
+        })))
+        }else if(order.status=="1"){
+            tr.append($("<td>").append(($("<button>").addClass("am-round")).text("配送").on("click",(e)=>{
+                e.preventDefault();
+            changeOrder(order,2)
+        })))
+        }else if(order.status=="2"){
+            tr.append($("<td>").append($("<button>").addClass("am-round").text("完成").on("click",(e)=>{
+                e.preventDefault();
+            changeOrder(order,3)
+        })))
+        }else if(order.status=="3"){
+            tr.append($("<td>").text("已完成"));
+        }else if(order.status=="4"){
+            tr.append($("<td>").text("已拒绝接单"));
+        }else {
+            tr.append($("<td>").text("接单已取消"));
+        }
+    })
     }
     function getOrderItem(id) {
-        alert(host+"/orderItems/"+id);
         $.ajax({
             type:'GET',
             url:host+"/orderItems/"+id,
             contentType: "application/json; charset=utf-8",
             dataType:"json",
             success:function (data) {
-                alert(21312)
-                $("#"+data.id).remove();
+                alert(data);
+                model=data;
+                renderOrderItem();
             }
         })
     }
-    
+
     function changeOrder(order,status) {
         order.status=status;
         $.ajax({
@@ -101,22 +102,17 @@ function CustomerComponent($view,host,mId,status) {
         })
 
     }
-    function selectCustomer(orders) {
-        cur=customer;
-        let cnameInput=$view.find("#updateForm input[name=cname]");
-        cnameInput.val(customer.cname);
-    }
     function timeStamp2String(time){
         if (time!=null){
-        var datetime = new Date();
-        datetime.setTime(time);
-        var year = datetime.getFullYear();
-        var month = datetime.getMonth() + 1 < 10 ? "0" + (datetime.getMonth() + 1) : datetime.getMonth() + 1;
-        var date = datetime.getDate() < 10 ? "0" + datetime.getDate() : datetime.getDate();
-        var hour = datetime.getHours()< 10 ? "0" + datetime.getHours() : datetime.getHours();
-        var minute = datetime.getMinutes()< 10 ? "0" + datetime.getMinutes() : datetime.getMinutes();
-        var second = datetime.getSeconds()< 10 ? "0" + datetime.getSeconds() : datetime.getSeconds();
-        return year + "-" + month + "-" + date+" "+hour+":"+minute+":"+second;
+            var datetime = new Date();
+            datetime.setTime(time);
+            var year = datetime.getFullYear();
+            var month = datetime.getMonth() + 1 < 10 ? "0" + (datetime.getMonth() + 1) : datetime.getMonth() + 1;
+            var date = datetime.getDate() < 10 ? "0" + datetime.getDate() : datetime.getDate();
+            var hour = datetime.getHours()< 10 ? "0" + datetime.getHours() : datetime.getHours();
+            var minute = datetime.getMinutes()< 10 ? "0" + datetime.getMinutes() : datetime.getMinutes();
+            var second = datetime.getSeconds()< 10 ? "0" + datetime.getSeconds() : datetime.getSeconds();
+            return year + "-" + month + "-" + date+" "+hour+":"+minute+":"+second;
         }else{
             return "未完成...";
         }
@@ -130,6 +126,17 @@ function CustomerComponent($view,host,mId,status) {
             case "4":return "拒绝";
             case "5":return "取消";
         }
+
+    }
+    function renderOrderItem() {
+        let $tbody=$view.find("#orderItem tbody");
+        $tbody.empty();
+        model.forEach((orderItem)=>{
+            $("<tr>").append($("<td>").text(orderItem.food.foodName))
+            .append($("<td>").text(orderItem.foodNum))
+            .append($("<td>").text(orderItem.totalPrice))
+            .appendTo($tbody);
+    })
 
     }
 }
